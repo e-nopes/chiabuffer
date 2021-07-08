46014970ca671998d96d72adb86993437035d86b
N0pes <nopes@nope.com>
2021-07-07 20:20:15 -0700

    job len 0 failure

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # chiabuffer
 
-Will spawn as many threads as you have destination disks, copies files to each destination in parallel
+Will spawn as many threads as you have destination disks (or a maximum), copies files to each destination in parallel
 
-Python script to monitor a Buffer drive and move plots to a dest drive. Will not include destination drives with less than `minSize` amount.
+Python script to monitor a Buffer drive and move plots to an array of dest drives. Will not include destination drives with less than `minSize` amount.
 
 Usage:
 
@@ -13,6 +13,7 @@ Settings to edit in .py file:
     minSize = 800 # Size in GB - if a dest drive has less free space remaining, it's removed from list
     sources = ["/mnt/from/"] # Comma separated list of dirs to monitor (/ at end is needed for linux!)
     destinations = ["/mnt/to/"] # comma separated list of dirs to move plots into (/ at end is needed for linux!)
+    max_concurrent = 4  # max number of parallel copies to spawn, regardless of destination
 
 
 
@@ -26,3 +27,8 @@ Notes:
     We scan the directory in sources for 'ext' files , and move files to a group of destinations.
     the files are more or less randomly thrown into any drive with available space.
     the intent is to parallelize the distribution, while only doing 1 copy at a time per destination.
+
+    It does no cleanup. hit ctrl-c 3 times at your own risk.
+    
+    If you have to ctrl-c out. rename your temp/*.plot.x.tmp files to .plot .
+    Also you should delete the partial plot.x.tmp file on your destination drive.
--- chiabuffer.py
+++ chiabuffer.py
@@ -15,6 +15,8 @@ ext = "plot"
 minSize = 120
 
 #override destination count with a constant
+
+#my buffer chokes out at 3 while network copying
 max_concurrent = 3
 
 # hit ctrl-c to stop the process. 3 times to forcibly break the moves.
@@ -27,16 +29,9 @@ sources = [f'/home/{user}/buffer/']
 
 #be sure to include the trailing / on the directory
 destinations = [
-                 f'/home/{user}/farm/sea34/'
-                ,f'/home/{user}/farm/sea21/'
-                ,f'/home/{user}/farm/sea10/'
-                ,f'/home/{user}/farm/sea36/'
-                ,f'/home/{user}/farm/sea37/'
-                ,f'/home/{user}/farm/sea38/'
-                ,f'/home/{user}/farm/sea39/'
-                ,f'/home/{user}/farm/sea40/'
-                ,f'/home/{user}/farm/sea41/'
                 ,f'/home/{user}/farm/sea43/'
+                ,f'/home/{user}/farm/sea44/'
+                ,f'/home/{user}/farm/sea45/'
                 ]
 
 jobs = []
@@ -47,13 +42,18 @@ file_count = -1
 throttle = 0
 ctrl_c_press_num = 0
 
+#lower this to print the destination list more often
+destination_print_mod = 10
+
+
 #verbosity
 v = False
 
 def move_one_plot ( source, dest, num=99 ) :
     # next lets scan the name, rename to plot.x.tmp, move, and rename again 
     tmp_ext = '.x.tmp' 
-    
+
+    # these should each be in their own try.    
     try:     
         filename = os.path.basename(source)
         destfilename = dest+filename
@@ -108,8 +108,12 @@ class JobPool():
     def worker(self, number ):
         while self.active:
             #deactivate when requested 
-            if (number >= self.usable):
-                time.sleep(300) #we are expecting a long sleep
+            if (number >= self.usable or number >= max_concurrent):
+                for i in range(10):
+                    if not self.active: 
+                        continue
+                    #maybe tweak this if you use a large number of concurrent threads
+                    time.sleep(3) #not too long, or we may not join quickly
                 continue
 
             #pull from queue, run next job
@@ -122,6 +126,7 @@ class JobPool():
                 print(e)
 
             print( datetime.now().strftime("%m/%d/%Y, %H:%M:%S")  + f' -- [{number}] Finished {source} -> {dest} ')
+
             self.q.task_done()
             
 def exit_gracefully(signum, frame):
@@ -134,42 +139,39 @@ def exit_gracefully(signum, frame):
 
     print(' please wait for threads to finish')
 
-    if ctrl_c_press_num < 3 :
-        remain = 3 - ctrl_c_press_num
+    if ctrl_c_press_num < 2 :
+        remain = 3 - ctrl_c_press_num 
         print(f' To Exit hit ctrl-c {remain} times. ')
         #keep our handler
         signal.signal(signal.SIGINT, exit_gracefully)
+    else:
+        print(' you will need to rename your .plot.x.tmp files to .plot , and probably delete the partial destination files too')
 
 def main( pool ):
     global stopSignal
     global file_count
     global throttle
+    global destination_print_mod
 
-    #print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + '  Main..Start')               
-
-
-    diskUsage = {}
     jobs.clear()
 
-    #no more jobs to be added
     if stopSignal:
         print('Time to Stop')
         return 
 
-
-    if throttle % 8 == 0:
+    if throttle % destination_print_mod == 0:
         print(f' -------------------------  ' )
     #fix this, awful hack to do this twice
     for dest in destinations:
         total, used, free = shutil.disk_usage(dest)
         #only add det if it has enough space. 
         if (free // (2 ** 30)) > minSize :
-
             total = total // (2**30)
             used =  used  // (2**30)
             free =  free  // (2**30)
 
-            if throttle % 8 == 0:
+
+            if throttle % destination_print_mod == 0:
                 print(f' Destination : {dest}  Free Space: {free} GB ' )
             jobs.append(dest)
 
@@ -179,27 +181,20 @@ def main( pool ):
     #scan sources, create file list 
     for source in sources:
         for filename in glob.glob(source + "*." + ext):
-            if stopSignal:
-                pass
+            if stopSignal or len(jobs)==0:
+                continue
 
             file_count += 1
             try:
                 destfilename = jobs[file_count % len(jobs)]
-                
-                #no doubles
                 pool.addJob( filename, destfilename)                
-            
-                #If we spawn too fast, we may add multiples before the job changes the filename
-                time.sleep(1)
+                time.sleep(1) # no need to spawn too fast
 
             # For other errors
             except (shutil.Error ):
                 print("Shutil OS Error occurred while copying filename.")
             except Exception as e: 
                 print(e)
-
-    #print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + '  Main..End')               
-
 
 
 
@@ -222,7 +217,7 @@ if __name__ == "__main__":
     pool = JobPool( size )
 
     print('Started Buffer Process: ' + current_time,' Max Num parallel: '+ str(size))
-    print('Press Ctrl+C 3x to exit, File currently in process will complete before exit.')
+    print('Press Ctrl+C to exit, File currently in process will complete before exit.')
 
     #sys.exit(0)
     while True:
@@ -238,6 +233,5 @@ if __name__ == "__main__":
             print(' Waiting for Thread pool cleanup')
             pool.stop()
             sys.exit(0)
-
 
 
